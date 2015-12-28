69efa88806030343ad76a02313747d2b5240889d
kimyongwon <ddyw3@nate.com>
2015-12-28 09:53:12 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
+//For command line git tutorial
+//GitHub Modification
 public class Hello {
-public static void main(String[] args){
-System.out.println("Hello Git!");
-System.out.println("what is git?");
-System.out.println("Thas's DVCS");
-System.out.println("2002 BitKeeper");
-}
+        public static void main(String[] args){
+                System.out.println("Hello Git!");
+                System.out.println("what is git?");
+                System.out.println("Thas's DVCS");
+                System.out.println("2002 BitKeeper");
+        }
 }
 
